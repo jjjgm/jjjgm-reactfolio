a014582aa20dc1bca4db3e0eaa9c42cfd8fb95aa
jjjgm <businessjgm@icloud.com>
2023-05-10 12:06:26 -0700

    form basics

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import { useState } from 'react';
 
 
 function Contact() {
-  return (
-    <div>Contact</div>
-  )
+  const [ input, setInput ] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInput(values => ({...values, [name]: value}));
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  alert(input)
+}
+
+// add a validator for email addresses
+
+return (
+<div>
+
+  <form className="contactForm" onSubmit={handleSubmit}>
+    <label>Your Name:
+    <input 
+      type="text" 
+      name="contactName" 
+      value={input.contactName|| ""} 
+      onChange={handleChange}
+    />
+    </label>
+    <label>Your email:
+      <input 
+        type="text" 
+        name="contactEmail" 
+        value={input.contactEmail || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <input type="submit" />
+  </form>
+
+  </div>
+
+
+);
 }
 
 export default Contact;
