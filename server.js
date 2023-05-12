1e0ccde616cee79a53b086bf7d6f0b35988b2d54
jjjgm <businessjgm@icloud.com>
2023-05-11 21:24:58 -0700

    removed bs from indexjs

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
 
-
 const PORT = process.env.port || 3001;
 
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
