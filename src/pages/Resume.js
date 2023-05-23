590f7b4208b8c2d68807b270ede5f56f20b21a60
jjjgm <businessjgm@icloud.com>
2023-05-23 16:34:16 -0700

    new commit

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -10,7 +10,7 @@ const Resume = () => {
   return (
     <div className='resume'>
       <div className="container">
-        <h3 className='resblurb'>If you would like to know more about my work experience, download my resume - Thanks!</h3>
+        <h3 className='resblurb'>If you would like to know more about my work experience, please download my resume - Thanks!</h3>
         <a href= "https://docs.google.com/document/d/e/2PACX-1vT_aLkfCxREhkGJx2kpC3Sopyw9674szpP9zzZzxRikSzZgvLXQK4zXVjEtZiQxy7wrUTIvHnssdK7Z/pub"><button className="download">Download My Resume</button></a>
       </div>
     </div>
